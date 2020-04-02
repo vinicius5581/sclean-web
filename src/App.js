63a789decf2ea9a3f555f4314e53c387b0ea8b4e
Vinicius Santana <santanaviny@gmail.com>
2020-04-01 21:10:49 -0700

    React Hooks Context Demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container } from "semantic-ui-react";
 
-function App() {
+import CounterView from "./views/counter-view";
+import ContactView from "./views/contact-view";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>React Hooks Context Demo</h1>
+      <CounterView />
+      <ContactView />
+    </Container>
   );
 }
-
-export default App;
