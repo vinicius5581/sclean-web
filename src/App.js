c91461885f9cf2739f442c2ad337efa0edf63e27
Vinicius Santana <santanaviny@gmail.com>
2020-04-04 02:11:20 -0700

    Build updates

--- src/App.js
+++ src/App.js
@@ -10,54 +10,55 @@ import AccountsView from "./views/accounts-view";
 import GithubCommit from "./components/github-commit";
 
 export default function App() {
+  console.log("process.env", process.env);
   return (
     <Container>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Menu>
-          <Menu.Item as={Link} to="/">
+          <Menu.Item as={Link} to={`/`}>
             Home
           </Menu.Item>
-          <Menu.Item as={Link} to="/accounts">
+          <Menu.Item as={Link} to={`/accounts`}>
             Accounts
           </Menu.Item>
-          <Menu.Item as={Link} to="/counter">
+          <Menu.Item as={Link} to={`/counter`}>
             Counter
           </Menu.Item>
-          <Menu.Item as={Link} to="/contacts">
+          <Menu.Item as={Link} to={`/contacts`}>
             Contacts
           </Menu.Item>
-          <Menu.Item as={Link} to="/githubapi">
+          <Menu.Item as={Link} to={`/githubapi`}>
             GitHub Api
           </Menu.Item>
-          <Menu.Item as={Link} to="/login">
+          <Menu.Item as={Link} to={`/login`}>
             Login
           </Menu.Item>
-          <Menu.Item as={Link} to="/signup">
+          <Menu.Item as={Link} to={`/signup`}>
             Sign up
           </Menu.Item>
         </Menu>
 
         <Switch>
-          <Route path="/accounts">
+          <Route exact path={`/`}>
+            <h1>Home</h1>
+          </Route>
+          <Route path={`/accounts`}>
             <AccountsView />
           </Route>
-          <Route path="/counter">
+          <Route path={`/counter`}>
             <CounterView />
           </Route>
-          <Route path="/contacts">
+          <Route path={`/contacts`}>
             <ContactView />
           </Route>
-          <Route path="/githubapi">
+          <Route path={`/githubapi`}>
             <GithubCommit />
           </Route>
-          <Route path="/login">
+          <Route path={`/login`}>
             <LoginView />
           </Route>
-          <Route path="/signup">
+          <Route path={`/signup`}>
             <SingupView />
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
           </Route>
         </Switch>
       </Router>
